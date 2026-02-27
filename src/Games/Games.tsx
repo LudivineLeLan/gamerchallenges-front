fa9d9339e86b3b069005b7a22bc08424fa7848b5
LudivineLeLan <lelanludivine@gmail.com>
2026-02-27 15:30:33 +0100

    pagination & games OK

--- src/Games/Games.tsx
+++ src/Games/Games.tsx
@@ -13,24 +13,29 @@ type Game = {
 const Games = () => {
 	const [games, setGames] = useState<Game[]>([]);
 	const [error, setError] = useState<string | null>(null);
+	const [page, setPage] = useState(1);
+	const [totalPages, setTotalPages] = useState(1);
 
 	const API_URL = import.meta.env.VITE_API_URL;
 
+	const fetchGames = async (pageNumber: number = 1) => {
+		try {
+			const res = await fetch(`${API_URL}/games?page=${pageNumber}&limit=6`);
+			if (!res.ok) throw new Error("Erreur lors de la récupération des jeux");
+
+			const data = await res.json();
+			console.log("Données reçues :", data);
+
+			setGames(data.games);
+			setPage(data.page);
+			setTotalPages(data.totalPages);
+		} catch (err) {
+			console.error(err);
+			setError("Impossible de charger les jeux.");
+		}
+	};
+
 	useEffect(() => {
-		const fetchGames = async () => {
-			try {
-				const res = await fetch(`${API_URL}/games`);
-				if (!res.ok) throw new Error("Erreur lors de la récupération des jeux");
-
-				const data = await res.json();
-				console.log("Données reçues :", data);
-				setGames(data.games); // tableau renvoyé par le back
-			} catch (err) {
-				console.error(err);
-				setError("Impossible de charger les jeux.");
-			}
-		};
-
 		fetchGames();
 	}, []);
 
@@ -39,14 +44,32 @@ const Games = () => {
 	return (
 		<div>
 			<h1>Liste des jeux</h1>
+
 			{games.map((game) => (
 				<Link key={game.id} to={`/games/${game.id}`}>
 					<img src={game.cover} alt={game.title} width={150} />
 					<h3>{game.title}</h3>
 				</Link>
 			))}
+
+			<div>
+				{page > 1 && (
+					<button type="button" onClick={() => fetchGames(page - 1)}>
+						Précédent
+					</button>
+				)}
+
+				<span>
+					Page {page} / {totalPages}
+				</span>
+
+				{page < totalPages && (
+					<button type="button" onClick={() => fetchGames(page + 1)}>
+						Suivant
+					</button>
+				)}
+			</div>
 		</div>
-    
 	);
 };
 
