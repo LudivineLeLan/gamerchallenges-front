c92247dec4b1b998d021e9682311ef71a38c4fde
Claude Ros <ros.claude.8@gmail.com>
2026-03-03 13:31:07 +0100

    Footer : mobile version

---
+++ src/ui/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className="bg-transparent mt-auto w-full bg-gray-100 py-4 text-center uppercase">
+      <div className="bg-[#034b57] rounded-full py-3 mx-3 font-bold text-[0.9rem] flex justify-center gap-6 mb-4">
+        <Link to="/a-propos">a propos</Link>
+        <Link to ="/mentions-legales">mentions legales</Link>
+        <Link to="/contact">contact</Link>
+      </div>  
+      <p className="text-white font-bold text-xs">
+        2026 - gamerschallenges
+      </p>
+    </footer>
+  );
+};
