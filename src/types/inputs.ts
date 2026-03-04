a10c1d2637dd593cfbc26615306d3885da50bfd6
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-03-04 15:14:11 +0100

    feat: type InputProps added to input.ts and imported in Input.tsx

--- src/types/inputs.ts
+++ src/types/inputs.ts
@@ -1,7 +1,9 @@
- export type InputProps = {
-    type: "text" | "email" | "checkbox" | "password";
+
+export type InputProps = {
+    type?: "text" | "email" | "checkbox" | "password";
+    isTextArea?: boolean;
     placeholder?: string;
-    onChange : (e : React.ChangeEvent<HTMLInputElement>) => void;
+    onChange : (e : React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
     checked? : boolean;
     value?: string;
     name: string;
--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -1,14 +1,4 @@
-import React from 'react'
-
-type InputProps = {
-    type?: "text" | "email" | "checkbox" | "password";
-    isTextArea?: boolean;
-    placeholder?: string;
-    onChange : (e : React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
-    checked? : boolean;
-    value?: string;
-    name: string;
-}
+import type { InputProps } from "../types/inputs"
 
 // type, isTextArea and checked are isolated props from the others props because they are not common to textarea and input
 export default function Input({ type, isTextArea, checked, ...rest } : InputProps) {
