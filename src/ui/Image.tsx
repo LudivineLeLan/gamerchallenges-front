c66ffc225ff70304882877f15be482abeb9b4647
LudivineLeLan <lelanludivine@gmail.com>
2026-03-04 15:41:40 +0100

    fixed Image component for padding & height ratio

--- src/ui/Image.tsx
+++ src/ui/Image.tsx
@@ -5,13 +5,15 @@ type ImageProps = {
 
 const Image = ({ src, alt = "" }: ImageProps) => {
 	return (
-		<div className="relative flex justify-center items-center overflow-visible w-[200px] h-[120px]">
-			<div className="absolute left-1/2 -translate-x-1/2 w-16 -top-3 -bottom-3 bg-green-medium opacity-100 blur-2xl rounded-2x1 z-0" />
+		<div className="relative flex justify-center items-center w-full max-w-[200px] aspect-[5/3] mx-auto">
+			{/* Fond flou */}
+			<div className="absolute left-1/2 -translate-x-1/2 w-1/4 -top-3 -bottom-3 bg-green-medium opacity-100 blur-2xl rounded-2xl z-0" />
 
+			{/* Image */}
 			<img
 				src={src}
 				alt={alt}
-				className="relative z-10 w-[200px] h-[120px] object-cover rounded-xl border-2 border-green-medium"
+				className="relative z-10 w-full h-full object-cover rounded-xl border-2 border-green-medium"
 			/>
 		</div>
 	);
