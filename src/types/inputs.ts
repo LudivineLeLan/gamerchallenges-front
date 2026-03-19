35b9edd667b97263e3f2780d8ed456d4e83f5e28
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-03-19 11:00:57 +0100

    feat: InputProps updated

--- src/types/inputs.ts
+++ src/types/inputs.ts
@@ -1,5 +1,4 @@
 export type InputProps = {
-<<<<<<< HEAD
     type?: "text" | "email" | "checkbox" | "password" | "url";
     isTextArea?: boolean;
     placeholder?: string;
@@ -9,16 +8,3 @@ export type InputProps = {
     name?: string;
     width?: string;
 }
-=======
-  type?: React.HTMLInputTypeAttribute;
-  isTextArea?: boolean;
-  placeholder?: string;
-  onChange: (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => void;
-  checked?: boolean;
-  value?: string;
-  name?: string;
-  width?: string;
-};
->>>>>>> dev
